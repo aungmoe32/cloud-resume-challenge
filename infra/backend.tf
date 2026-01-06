55390dc9a388956a32dce0d7a90761808d202b17
Aung Moe Myint Thu <aungmoemyintthu@gmail.com>
2026-01-06 15:53:50 +0630

    Fix Terraform backend region to use static value "ap-southeast-1"

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "cloudresume-terraform-state"
     key            = "project.tfstate"
-    region         = var.aws_region
+    region         = "ap-southeast-1"
     dynamodb_table = "cloudresume-terraform-state"
     encrypt        = true
   }
